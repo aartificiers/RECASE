64aa66d271e83e6f9634e531dab36b4728bce120
aartificiers <artify.aartificiers@gmail.com>
2023-09-23 21:43:23 +0530

    dwewefew

--- src/Pages/Admin/Dashboard/Dash/Dash.js
+++ src/Pages/Admin/Dashboard/Dash/Dash.js
@@ -4,20 +4,24 @@ import { API } from '../../../../Services/Api';
 import DashHeader from '../../../../Components/AdminComponents/HeaderCompponent/DashHeader';
 import { FaEdit, FaSave } from 'react-icons/fa';
 import QuillEditor from '../../../../Components/List/Quill/QuillEditor';
-import { addsData } from '../../../../Constants/dummy';
+import { addsData, netWeeklyData } from '../../../../Constants/dummy';
 
 const Dash = () => {
     const [luckyNum, setLuckyNum] = useState({ shubhank: "", finalank: [] });
     const [ads, setAds] = useState([]);
-    const [adsUpdateData, setUpdateAds] = useState({adContent: ""});
+    const [netWeekly, setNetWeekly] = useState([]);
+    const [dayNight, setDayNight] = useState([1,2,3,4]);
+    const [adsUpdateData, setUpdateAds] = useState({ adContent: "" });
     const [luckynumedit, setLuckyNumedit] = useState(false);
-    const [adEdit, setAdEdit] = useState(false);
-  
+    const [adEdit, setAdEdit] = useState(null);
+    const [netEdit, setNetEdit] = useState(null);
+    const [dnGuessingEdit, setDnGuessingEdit] = useState(null);
+
     useEffect(() => {
         fetchLuckyNum();
         fetchAds();
     }, []);
-     
+
     const fetchLuckyNum = async () => {
         const res = await API.getluckyNum({ id: "6504c1e8c0972c9a038dd5a2" });
         if (res.isSuccess) {
@@ -70,7 +74,7 @@ const Dash = () => {
 
     }
 
-    const handleAdSave = async (id,updateData) => {
+    const handleAdSave = async (id, updateData) => {
 
         const resp = await API.editAds({ id, updateData });
 
@@ -82,11 +86,11 @@ const Dash = () => {
             console.log("updation failed");
         }
     }
-     const handleQuilChange = (adContent) =>{
+    const handleQuilChange = (adContent) => {
 
-        setUpdateAds({adContent});
+        setUpdateAds({ adContent });
         console.log(adContent);
-     }
+    }
 
     return (
         <div className="stats">
@@ -105,23 +109,63 @@ const Dash = () => {
                         {luckynumedit ? <input type="text" value={luckyNum.finalank.join(",")} name='finalank' onChange={handleLuckyChange} /> : luckyNum.finalank?.length > 0 ? luckyNum.finalank.join(',') : null}
                     </div>
                     <div className="btns">
-                        {luckynumedit ? <button onClick={() => handleLuckySave(luckyNum._id)}><FaSave /></button> : <button onClick={() => setLuckyNumedit(true)} ><FaEdit /></button>}
+                        {luckynumedit ? <button className='btn btn__primary' onClick={() => handleLuckySave(luckyNum._id)}><FaSave /></button> : <button className='btn btn__primary' onClick={() => setLuckyNumedit(true)} ><FaEdit /></button>}
 
 
                     </div>
                 </div>
+
                 <div className='adds-edit'>
+                    <h1 className='sec-title'>Ads</h1>
                     <div className="adds-grid">
-                        {ads.length> 0 ? ads.map((item, index) => {
+                        {ads.length > 0 ? ads.map((item, index) => {
                             return (
-                                <div key={index} className="adds">
+                                <div key={index} className="adds-view">
                                     <div className='ads-heads'>
                                         <h1>Ad {index + 1} </h1>
-                                      { adEdit !== index ? <button onClick={() => setAdEdit(index)}> <FaEdit /> </button> : <button onClick={() => handleAdSave(item._id , adsUpdateData)}> <FaSave /> </button> }
+                                        {adEdit !== index ? <button onClick={() => setAdEdit(index)}> <FaEdit /> </button> : <button onClick={() => handleAdSave(item._id, adsUpdateData)}> <FaSave /> </button>}
                                     </div>
                                     {adEdit === index ? <QuillEditor onChange={handleQuilChange} value={item.adContent} /> : <div dangerouslySetInnerHTML={{ __html: item.adContent }}></div>}
                                 </div>
                             )
+                        }) : null}
+                    </div>
+                </div>
+
+                <div className="netweekly-edit">
+                    <h1 className="sec-title">Net Weekly</h1>
+                    <div className="net-week-wrap">
+                        {netWeeklyData.map((item, indx) => {
+                            return (
+                                <div className="net-card" key={indx}>
+                                    <div className="net-title">{netEdit == indx ? <input style={{ width: "100%", height: "35px", fontSize: "1.2em" }} type="text" value={"hello"} /> : "hello"}</div>
+                                    {netEdit === indx ? <QuillEditor value={"duibucucdh"} onChange={() => console.log("dftydc")} /> : <div className="netcontent" dangerouslySetInnerHTML={{ __html: "<p>Hello</p>" }}></div>}
+                                    <div className="btns-cont">
+                                        {netEdit === indx ? <button className='btn btn__primary' onClick={() => setNetEdit(null)} ><FaSave /></button> : <button className='btn btn__primary' onClick={() => setNetEdit(indx)} ><FaEdit /></button>}
+                                    </div>
+
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+
+                <div className="daynight-guessing">
+                    <h1 className="sec-title">Day Night Guessing</h1>
+                    <div className="dynt-grid">
+                        {dayNight.length > 0 ? dayNight.map((item, index) => {
+                            return (
+                                <div className="dynt-item">
+                                    <div className="ttl">Milan MOrning</div>
+                                    {dnGuessingEdit === index ? <QuillEditor/> : <div className="guess-cont" >dgsgdssdsdsdsdgsd vsdgsdgsd bsgsd sdgsdgs fbsff df df d df df df df </div>}
+                                    <div className="btn-cont">
+                                        {dnGuessingEdit === index ? <button onClick={()=>setDnGuessingEdit(null)} ><FaSave/></button> : <button onClick={()=>setDnGuessingEdit(index)}><FaEdit /></button>}
+                                        
+                                    </div>
+                                </div>
+                            )
+
                         }):null}
                     </div>
                 </div>
