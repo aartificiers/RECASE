57f2a456c80bd98c3c64f1e5c639d46c6e251e61
aartificiers <artify.aartificiers@gmail.com>
2023-10-16 01:33:18 +0530

    vdvvdzv

--- src/Components/AdminComponents/HeaderCompponent/DashHeader.js
+++ src/Components/AdminComponents/HeaderCompponent/DashHeader.js
@@ -1,34 +1,54 @@
-import React from 'react';
-import { FaSearch, FaUser } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaHome, FaMinus, FaSearch, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { BsDice6 } from 'react-icons/bs';
 import "./Header.scss";
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import { BiSolidDashboard } from 'react-icons/bi';
 import { IoGameController } from 'react-icons/io5';
+import { GrAdd } from 'react-icons/gr';
+import { addUser } from '../../../Store/Slices/userSlice';
+import { logoutUser } from '../../../Utils/commonutil';
+import { IoIosAdd, IoMdExit } from 'react-icons/io';
 
 const DashHeader = (props) => {
-    const user=useSelector(state=>state.user);
+    const user = useSelector(state => state.user);
+    const [openNavLinks,setOpenNavLinks]=useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const logout = () => {
+        if (logoutUser()) {
+            dispatch(addUser({ isAuthenticated: false, user: {} }));
+            navigate("/admin/login");
+        } else {
+            toast.error("Logout Failed");
+        }
+    }
     return (
         <div className="admin-header">
             <div className="headerWrap">
                 <div className="title">{props.title}</div>
                 <div className="searchbar">
                     <input type="text" />
-                    <button><FaSearch/></button>
+                    <button><FaSearch /></button>
                 </div>
-                <div className="nav-btn"><button><BsDice6/></button></div>
+                <div className="nav-btn"><button><BsDice6 /></button></div>
                 <div className="bottom-navigation">
                     <div className="bot-nav-links">
-                        {user.user.role==="BENJO" && <div className="link"><Link to={'/admin/dashboard/dash'} ><BiSolidDashboard/></Link></div>}
-                        {user.user.role === "BENJO" && <div className="link"><Link to={'/admin/dashboard/subuser'} ><FaUser/></Link></div>}
-                        <div className="link"><Link to={'/admin/dashboard/games'} ><IoGameController/></Link></div>
+                        <div className={openNavLinks ? "collapsible active" : "collapsible"}>
+                            {user.user.role === "BENJO" && <div className="link"><Link to={'/admin/dashboard/dash'} ><BiSolidDashboard /></Link></div>}
+                            {user.user.role === "BENJO" && <div className="link"><Link to={'/admin/dashboard/subuser'} ><FaUser /></Link></div>}
+                            <div className="link"><Link to={'/admin/dashboard/games'} ><IoGameController /></Link></div>
+                            <div className="link"><Link to={'/'} ><FaHome /></Link></div>
+                            <div className="link"><button onClick={logout} className='logBtn'><IoMdExit /></button></div>
+                        </div>
+                        <div className="link">{openNavLinks ? <button onClick={()=>setOpenNavLinks(false)} className='logBtn'><FaMinus/></button>: <button onClick={()=>setOpenNavLinks(true)} className='logBtn'><IoIosAdd /></button>}</div>
                     </div>
-
                 </div>
             </div>
         </div>
-      )
+    )
 }
 
 export default DashHeader
